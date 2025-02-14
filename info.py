068d1740ef610310ea97cfcdd396e87d70ff7b10
Kokopw <kmelon175@gmail.com>
2025-02-14 13:07:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'True')), False)
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://gebit25986:vse9d92xg3zFpSim@cluster0.yivso.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Gebits")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Safaribotts')
 
 #stream link shortner
